67453134af608600a0bd4265eab2d06266599815
Amrit747 <mrtkhnl@gmail.com>
2025-02-17 20:44:57 +0545

    simple code to check if a number entered by user is odd or even

---
+++ odd_or_even.py
@@ -0,0 +1,6 @@
+#program that checks if a number entered by user is even or odd
+first_number = float(input("Enter a number"))
+if first_number%2 == 0:
+    print("Number is even")    
+else:
+    print("Number is odd") 
